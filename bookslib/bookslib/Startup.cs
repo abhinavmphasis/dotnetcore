5a4d2c95e48cf76d952e21d498b5cd15c05aa066
Abhinav <abhinavkumar68@gmail.com>
2020-09-30 07:16:09 +0530

    Added MVC to the application

--- bookslib/bookslib/Startup.cs
+++ bookslib/bookslib/Startup.cs
@@ -16,6 +16,7 @@ namespace bookslib
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,19 @@ namespace bookslib
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.Map("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World");
+                //});
+
+                // add the controller to the routing module 
+                endpoints.MapDefaultControllerRoute();
             });
+    
         }
     }
 }
