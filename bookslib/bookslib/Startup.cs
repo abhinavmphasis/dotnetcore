feeeec8e8ec9da47a7bbdb38d011e9418793e7cf
Abhinav <abhinavkumar68@gmail.com>
2020-10-05 18:02:07 +0530

    dbcontext class added

--- bookslib/bookslib/Startup.cs
+++ bookslib/bookslib/Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using bookslib.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -18,7 +20,9 @@ namespace bookslib
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreContext>(options => options.UseSqlServer("Server =.; Database = BookStore; Integrated Security = true; "));
             services.AddControllersWithViews();
+
 #if DEBUG
 
             services.AddRazorPages().AddRazorRuntimeCompilation();
